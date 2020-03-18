031e5c01ccc50d5020545dfbd4baff0b10f34b71
Rennan Oliveira <rennan_neves@live.com>
2020-03-18 14:30:11 -0300

    Listagem de provedores de serviço

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import multerConfig from './config/multer';
 
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', authMiddleware, UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
