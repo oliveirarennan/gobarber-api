bdf109b5e60192ddaebde9290001011bafa231d5
Rennan Oliveira <rennan_neves@live.com>
2020-03-18 17:51:17 -0300

    Pagination and date operations

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import multerConfig from './config/multer';
 
@@ -21,6 +23,10 @@ routes.use(authMiddleware);
 routes.put('/users', authMiddleware, UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
